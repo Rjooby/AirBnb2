d3390658f23b5dbbf9a660fdfb756dee75677e0e
Ron Joo <rjoo189@gmail.com>
2015-01-12 09:08:38 -0500

    fixes request page

--- app/assets/javascripts/views/requests/requests_index.js
+++ app/assets/javascripts/views/requests/requests_index.js
@@ -2,6 +2,10 @@ Air.Views.RequestsIndex = Backbone.View.extend({
 
   template: JST['requests/index'],
 
+  initialize: function () {
+    this.listenTo(this.model, "sync", this.render);
+  },
+
   render: function () {
     console.log(this.model);
     var content = this.template({ location : this.model});
